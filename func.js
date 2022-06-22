7444863f498f62255efd90024f9dc34fac4a747c
Pal-Mel <54739062+Pal-Mel@users.noreply.github.com>
2022-06-22 14:57:16 +0300

    master

---
+++ func.js
@@ -0,0 +1,28 @@
+
+// сортування массиву за сумою
+function SortArray(x, y) {
+  if (Number(x["summa"]) > Number(y["summa"])) {
+    return -1;
+  }
+  if (Number(x["summa"]) < Number(y["summa"])) {
+    return 1;
+  }
+  return 0;
+}
+
+// заповнення клітинок на картинці
+function fillSectors(name) {
+  var curHer = dt[name];
+  curHer = curHer.sort(SortArray);
+  for (i in curHer) {
+    console.log(curHer[i]);
+  }
+  for (let i = 1; i <= 100; i++) {
+    for (let j = 1; j <= 100; j++) {
+      var d = document.getElementById("mainDiv");
+      var nEl = document.createElement("div");
+      nEl.className = "fillDiv paySector";
+      d.appendChild(nEl);
+    }
+  }
+}
