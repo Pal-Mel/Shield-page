0106030ffe894b391416b9637372f3a5eb8f5dec
Pal-Mel <54739062+Pal-Mel@users.noreply.github.com>
2022-06-22 15:49:35 +0300

    master

--- func.js
+++ func.js
@@ -1,4 +1,3 @@
-
 // сортування массиву за сумою
 function SortArray(x, y) {
   if (Number(x["summa"]) > Number(y["summa"])) {
@@ -20,8 +19,14 @@ function fillSectors(name) {
   for (let i = 1; i <= 100; i++) {
     for (let j = 1; j <= 100; j++) {
       var d = document.getElementById("mainDiv");
-      var nEl = document.createElement("div");
-      nEl.className = "fillDiv paySector";
+      var nEl = document.createElement("span");
+      nEl.className = "tooltip";
+      nEl.delayOpen = "50"
+      nEl.title = "Element"; // tool tip text
+      var sEl = document.createElement("div");
+      sEl.className = "fillDiv paySector";
+      nEl.appendChild(sEl);
+
       d.appendChild(nEl);
     }
   }
