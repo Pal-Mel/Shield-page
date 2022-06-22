b7453b29b0f534a8c1d69a717bc80266e1c8e52f
Pal-Mel <54739062+Pal-Mel@users.noreply.github.com>
2022-06-22 16:56:17 +0300

    master

--- func.js
+++ func.js
@@ -16,23 +16,30 @@ function fillSectors(name) {
   for (i in curHer) {
     console.log(curHer[i]);
   }
-  const countOfRow = 100
-  const countOfColumn = 100
+  const countOfRow = 100;
+  const countOfColumn = 100;
 
-  var h = 0
-  var nEl
+  var h = 0;
+  var nEl;
   var d = document.getElementById("mainDiv");
-console.log(d.clientWidth)
 
   for (let i = 1; i <= countOfRow; i++) {
     for (let j = 1; j <= countOfColumn; j++) {
-
-      nEl = document.createElement("span");
-      nEl.className = "tooltip fillDiv paySector";
-      nEl.delayOpen = "10"
-      nEl.title = "Element"; // tool tip text
-     
-      d.appendChild(nEl);
+        
+      if (h < curHer.length-1) {
+        var width = 6 * curHer[h]["count"] - 1;
+        nEl = document.createElement("span");
+        nEl.style.width = width.toString() + "px";
+        nEl.className = "tooltip fillDiv paySector";
+        nEl.delayOpen = "10";
+        nEl.title = curHer[h]["Name"] + " - " + curHer[h]["summa"] + " грн"; // tool tip text
+        d.appendChild(nEl);
+        h += 1;
+      } else {
+        nEl = document.createElement("span");
+        nEl.className = "fillDiv noPaySector";
+        d.appendChild(nEl);
+      }
     }
   }
 }
