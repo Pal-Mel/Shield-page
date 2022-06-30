8f871beeb1a8716143c61339033e5202683c0f34
Pal-Mel <54739062+Pal-Mel@users.noreply.github.com>
2022-06-30 10:44:56 +0300

    master

--- func.js
+++ func.js
@@ -19,43 +19,51 @@ function fillSectors(name) {
   var h = 0;
   var countForFill = 0;
   var curWidthRow = 0;
+  var width 
 
   let j = 1;
   do {
-    if (h < curHer.length - 1) {
-      countForFill = curHer[h]["count"] * 1;
-
-      var width = widthBlock * countForFill;
-
-      if (curWidthRow + width > countOfColumn * widthBlock) {
-        if ((countOfColumn * widthBlock - curWidthRow) != 0) {
-          createBlock(
-            countOfColumn * widthBlock - curWidthRow,
-            true,
-            curHer[h]
-          );
-        }
-
-        var qq = width - (countOfColumn * widthBlock - curWidthRow);
-        curWidthRow = qq;
-        if (qq != 0) {
-          createBlock(qq, true, curHer[h]);
-        }
-      } else {
-        createBlock(width, true, curHer[h]);
-        curWidthRow += width;
-      }
-
-      h += 1;
-      j += countForFill;
-    } else {
-      createBlock(width, false, null,name);
+    console.log(curHer.length);
+    if (curHer.length == 0) {
+      width = widthBlock
+      createBlock(width, false, null, name);
       j += 1;
+    } else {
+      if (h < curHer.length - 1) {
+        countForFill = curHer[h]["count"] * 1;
+
+        width = widthBlock * countForFill;
+
+        if (curWidthRow + width > countOfColumn * widthBlock) {
+          if (countOfColumn * widthBlock - curWidthRow != 0) {
+            createBlock(
+              countOfColumn * widthBlock - curWidthRow,
+              true,
+              curHer[h]
+            );
+          }
+
+          var qq = width - (countOfColumn * widthBlock - curWidthRow);
+          curWidthRow = qq;
+          if (qq != 0) {
+            createBlock(qq, true, curHer[h]);
+          }
+        } else {
+          createBlock(width, true, curHer[h]);
+          curWidthRow += width;
+        }
+
+        h += 1;
+        j += countForFill;
+      } else {
+        createBlock(width, false, null, name);
+        j += 1;
+      }
     }
   } while (j <= countOfColumn * countOfRow);
 }
 
-function createBlock(width, payed, her = null,name="") {
+function createBlock(width, payed, her = null, name = "") {
   var d = document.getElementById("mainDiv");
   var nEl;
   nEl = document.createElement("span");
@@ -68,19 +76,18 @@ function createBlock(width, payed, her = null,name="") {
     // nEl.content = " - " + her["summa"] + " грн"; // tool tip text
     d.appendChild(nEl);
   } else {
-     nEl = document.createElement("a");
-    
+    nEl = document.createElement("a");
+
     // var modal = document.getElementById("myModal");
-    nEl.href="https://shop.spgr.org.ua/"+name+"/"
-    nEl.target="blank"
+    nEl.href = "https://shop.spgr.org.ua/" + name + "/";
+    nEl.target = "blank";
     nEl.className = "fillDiv noPaySector";
-    nEl.title ='Натисни! Допоможи ЗСУ. Купи частину автомобіля. Залиш своє ім’я на сайті'
+    nEl.title =
+      "Натисни! Допоможи ЗСУ. Купи частину автомобіля. Залиш своє ім’я на сайті";
     // nEl.onclick = function() {
     //   modal.style.display = "block";
     // }
-    
+
     d.appendChild(nEl);
   }
 }
-
-
